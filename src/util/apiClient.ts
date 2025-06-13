cc59a2636c12c99edf0454fa76b775506d9de2ae
Firdovsirz <146226087+Firdovsirz@users.noreply.github.com>
2025-06-13 17:14:54 +0400

    Update apiClient.ts

--- src/util/apiClient.ts
+++ src/util/apiClient.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import { RootState, store } from "../redux/store";
 
-const API_BASE_URL = 'http://127.0.0.1:5000';
+const API_BASE_URL = 'http://10.0.26.35:8000';
 
 const apiClient = axios.create({
   baseURL: API_BASE_URL,
